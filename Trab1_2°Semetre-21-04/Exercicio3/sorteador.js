d7000cef8558c6de9369d49b0fe64cf83075d135
Marcos Vinicius Esteves <111775524+MarcosEsteves18@users.noreply.github.com>
2023-04-21 12:47:56 -0300

    Delete Trab1_2°Semetre-21-04 directory

--- Trab1_2°Semetre-21-04/Exercicio3/sorteador.js
+++
@@ -1,6 +0,0 @@
-function sortear(){
-    let min = parseInt(document.getElementById("min").value);
-    let max = parseInt(document.getElementById("max").value);
-    let sorteado = parseInt(Math.random() * (max - min) + min);
-    document.getElementById("sorteado").innerHTML = sorteado;
- }
